1e46680dd92a023742d6c8ded71bcbdde41e85ce
Deepak Guru <deepak.kamesh@gmail.com>
2022-07-17 21:26:17 -0700

    major file refactor; handler is not a lib to be called from multiple HW revs

---
+++ Board_Firmware/18f26K22-rev0.01.X/build/default/production/_ext/1360930230/handler_lib.p1.d
@@ -0,0 +1,13 @@
+build/default/production/_ext/1360930230/handler_lib.p1:  \
+../lib/handler_lib.c  \
+../lib/handler_lib.h  \
+../lib/master_exports.h.h  \
+../lib/../18f26K22-rev0.01.X/exports.h  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/spi1.h  \
+mcc_generated_files/tmr1.h  \
+../lib/nrf24_lib.h  \
+../lib/dht11_lib.h 
